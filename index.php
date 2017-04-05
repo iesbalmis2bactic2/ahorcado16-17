f79b4cec0427c5731d866a98eda66645a2298e80
juanjobalmis <juanjo@iesdoctorbalmis.com>
2017-04-05 11:53:54 +0200

    Dividir renderizado página del juego

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
+include 'definition.php';
+
+function MuestraEstadoDelJuego(
+$definicion, $imagen, $palabra, $letrasAcertadas, $letrasFalladas) {
+    $MaxNumFallos = 6;
+
+    MuestraDefinicion($definicion, $imagen);
+    echo "Palabra: ";
+    for ($index = 0; $index < count($palabra); $index++) {
+        $letra = $palabra[$index];
+        if (array_search($letra, $letrasAcertadas) === false) {
+            echo "_ ";
+        } else {
+            echo $letra." ";
+        }
+    }
+    echo "<br />";
+    echo "Falladas: ";
+    for ($index = 0; $index < count($letrasFalladas); $index++) {
+        $letra = $letrasFalladas[$index];
+        echo "{$letra}, ";
+    }
+    echo "<br />";
+    MostrarFormulario();
+    $imagenFallo=MuestraImagenesFallos($letrasFalladas);
+    
+    echo $imagenFallo;
+    
+}
 
 function EstaLetraEnIntroducidas($letra, $letrasAcertadas, $letrasFalladas) {
     $letraIntroducida = !(array_search($letra, $letrasAcertadas) == false && array_search($letra, $letrasFalladas) == false);
@@ -55,36 +84,6 @@ function MuestraImagenesFallos($letrasFalladas)
  
  return $imagenFallo;
  
-}
-
-
-function MuestraEstadoDelJuego(
-$definicion, $imagen, $palabra, $letrasAcertadas, $letrasFalladas) {
-    $MaxNumFallos = 6;
-
-    echo "Definicion: $definicion <br />";
-    echo "Imagen: $imagen <br />";
-    echo "Palabra: ";
-    for ($index = 0; $index < count($palabra); $index++) {
-        $letra = $palabra[$index];
-        if (array_search($letra, $letrasAcertadas) === false) {
-            echo "_ ";
-        } else {
-            echo $letra." ";
-        }
-    }
-    echo "<br />";
-    echo "Falladas: ";
-    for ($index = 0; $index < count($letrasFalladas); $index++) {
-        $letra = $letrasFalladas[$index];
-        echo "{$letra}, ";
-    }
-    echo "<br />";
-    MostrarFormulario();
-    $imagenFallo=MuestraImagenesFallos($letrasFalladas);
-    
-    echo $imagenFallo;
-    
 }
 
 function EstableceDatosPartida() {
