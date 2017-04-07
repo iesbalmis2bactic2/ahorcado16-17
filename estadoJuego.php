32919d869c48f1e1d8b9ff948f6bb87443c91481
alegonzalez4 <gonzalez4alex.10@gmail.com>
2017-04-07 06:18:27 +0200

    cambios Jandro

--- estadoJuego.php
+++ estadoJuego.php
@@ -5,6 +5,33 @@ include 'ImagenesFallos.php';
 include 'palabra.php';
 include 'fallos.php';
 
+function MostrarFormulario() {
+    ?>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="get">
+        Introduce una letra: <input name="letra" value="" type="text"><br />
+        <input name="aceptar" value="Aceptar" type="submit">
+    </form>
+    <?php
+}
+
+function MuestraImagenesFallos($letrasFalladas)
+{
+    $numero= count($letrasFalladas);
+ $imagenesFallos= array(
+        "./imagenFallo/cero.png",
+        "./imagenFallo/uno.png",
+        "./imagenFallo/dos.png",
+        "./imagenFallo/tres.png",
+        "./imagenFallo/cuatro.png",
+        "./imagenFallo/cinco.png",
+        "./imagenFallo/seis.png");
+ 
+ $imagenFallo=$imagenesFallos[$numero];
+ 
+ return $imagenFallo;
+ 
+}
+
 function MuestraEstadoDelJuego(
 $definicion, $imagen, $palabra, $letrasAcertadas, $letrasFalladas) {
 
